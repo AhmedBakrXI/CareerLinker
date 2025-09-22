f19a6eabd05ad449eba02d939ae594a649c644a8
AhmedBakrXI <ahmedbakr2501@gmail.com>
2025-09-22 23:58:06 +0300

    Add initial project setup with Maven wrapper and application properties

---
+++ resume/src/test/java/com/a7/job/resume/ResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a7.job.resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
